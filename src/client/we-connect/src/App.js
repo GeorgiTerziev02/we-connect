cc1b8aba22367251ad1193fda697913b99b96a1c
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2020-07-13 18:09:01 +0300

    feat: Add footer component

--- src/client/we-connect/src/App.js
+++ src/client/we-connect/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Header from './components/header'
 import './App.css';
+import Header from './components/header'
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Footer />
     </div>
   );
 }
