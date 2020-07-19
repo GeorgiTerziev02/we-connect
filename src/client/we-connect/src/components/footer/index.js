713030838f5a1074c72bae97dfbf09429713d603
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2020-07-19 19:12:56 +0300

    fix: Add margin top to footer

--- src/client/we-connect/src/components/footer/index.js
+++ src/client/we-connect/src/components/footer/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import styles from './index.module.css'
 import { Link } from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className={styles.footer}>
             <Navbar sticky="bottom" fixed="bottom" bg="dark" variant="dark">
                 <Navbar.Brand as={Link} to="/">
                     We Connect
