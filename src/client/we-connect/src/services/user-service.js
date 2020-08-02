8d0e9392e059ab15b8037c375fae6d4abfa63353
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2020-08-02 19:41:26 +0300

    Reuse login/register functionality

--- src/client/we-connect/src/services/user-service.js
+++ src/client/we-connect/src/services/user-service.js
@@ -1,37 +1,27 @@
 const userService = {
-    // TODO: Code reuse !!!
     login: async (username, password) => {
-        const promise = await fetch('http://localhost:4000/api/login', {
-            method: 'POST',
-            body: JSON.stringify({
-                'username': username,
-                'password': password
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-
-        const data = await promise.json()
-
-        return data
+        return await auth(username, password, 'login')
     },
     register: async (username, password) => {
-        const promise = await fetch('http://localhost:4000/api/register', {
-            method: 'POST',
-            body: JSON.stringify({
-                'username': username,
-                'password': password
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-
-        const data = await promise.json()
-
-        return data
+        return await auth(username, password, 'register')
     }
+}
+
+const auth = async (username, password, type) => {
+    const promise = await fetch(`http://localhost:4000/api/${type}`, {
+        method: 'POST',
+        body: JSON.stringify({
+            'username': username,
+            'password': password
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    const data = await promise.json()
+
+    return data
 }
 
 export default userService
