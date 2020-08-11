1468a06ea5e3f1262c3481d86d9d2f30ac28849b
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2020-08-11 22:31:45 +0300

    Create comment functionality

--- src/client/we-connect/src/components/add-comment/index.js
+++ src/client/we-connect/src/components/add-comment/index.js
@@ -2,18 +2,52 @@ import React, { useState } from 'react'
 import styles from './index.module.css'
 import SubmitButton from '../button/submit-button'
 import TextArea from '../text-area'
+import commentService from '../../services/comment-service'
+import { useHistory } from 'react-router-dom'
+import ErrorMessage from '../error-message'
 
 const AddComment = ({ postId }) => {
     const [content, setContent] = useState('')
     const [contentError, setContentError] = useState(false)
     const [contentErrorMessage, setContentErrorMessage] = useState('')
+    const [submitError, setSubmitError] = useState(false)
+    const [submitErrorMessage, setSubmitErrorMessage] = useState('')
+
+    const history = useHistory()
 
     const handleContentBlur = () => {
-        
+        if (!content) {
+            setContentError(true)
+            setContentErrorMessage('Comment content is required!')
+        } else if(content.length > 100){
+            setContentError(true)
+            setContentErrorMessage('Comment length must be less than 100 symbols!')
+        } else {
+            setContentError(false)
+            setContentErrorMessage('')
+        }
+    }
+
+    const submitHandler = async (event) => {
+        event.preventDefault();
+
+        handleContentBlur()
+
+        if (!contentError) {
+            const data = await commentService.create(postId, content)
+
+            if (data.error) {
+                setSubmitError(true)
+                setSubmitErrorMessage(data.error)
+            } else {
+                history.push(`/posts/${postId}`)
+            }
+        }
     }
 
     return (
-        <form className={styles["input-form"]}>
+        <form className={styles["input-form"]} onSubmit={submitHandler}>
+            <ErrorMessage error={submitError} errorMessage={submitErrorMessage} />
             <TextArea
                 id="content"
                 name="content"
---
+++ src/client/we-connect/src/services/post-service.js
@@ -0,0 +1,5 @@
+const postService = {
+    create: async () => { }
+}
+
+export default postService
