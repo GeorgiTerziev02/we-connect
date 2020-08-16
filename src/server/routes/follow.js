25a50ebac15b38c52ab6628b3d0f975aca7dad9f
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2020-08-16 15:52:14 +0300

    following functionality on the server

--- src/server/routes/follow.js
+++ src/server/routes/follow.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { authenticate } = require('../utils/auth');
-const { followUserById } = require('../controllers/follows');
+const { followUserById, getFollowing } = require('../controllers/follows');
 
 const router = Router();
 
@@ -14,5 +14,15 @@ router.post('/:id', authenticate, async (req, res) => {
         .status(result.error ? 400 : 201)
         .json(result);
 });
+
+router.get('/following', authenticate, async (req, res) => {
+    const userId = req.userId
+    
+    const result = await getFollowing(userId)
+
+    return res
+        .status(result.error ? 400 : 200)
+        .json(result)
+})
 
 module.exports = router;
